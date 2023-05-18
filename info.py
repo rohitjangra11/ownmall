f8ad2dd4d98f6a3a90a3a271e0606f5f3bd4f014
rohitjangra11 <126782133+rohitjangra11@users.noreply.github.com>
2023-05-18 16:51:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -23,6 +23,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 PICS = (environ.get('PICS', 'https://telegra.ph/Lucifer-05-18-66')).split()
 NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/46443096bc6895c74a716.jpg")
+MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/451f038b4e7c2ddd10dc0.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/5e2d4418525832bc9a1b9.jpg")
 
 # Admins, Channels & Users
