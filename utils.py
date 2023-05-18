a0baf83b532beb9de3c17e7d40d0ee0305b6e845
rohitjangra11 <126782133+rohitjangra11@users.noreply.github.com>
2023-05-18 14:43:36 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -643,10 +643,7 @@ async def send_all(bot, userid, files, ident):
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                        InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
-                        InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
-                    ],[
-                        InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/creatorbeatz")
+                        InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Botadmin44")
                         ]
                     ]
                 )
